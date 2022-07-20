83b5c790bb0e46646f4a63c62368d57c7cd19bea
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-07-20 09:12:24 +0530

    fresh fruits complete design

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,77 @@
+import React from "react";
+import "./Home.css";
+import img1 from "../../Sources/Images/online-grocery-gurgaon.jpg";
+import img2 from "../../Sources/Images/3-2-vegetable-transparent.png";
+import img3 from "../../Sources/Images/pulses.png";
+import img4 from "../../Sources/Images/fruit-1.png";
+import img5 from "../../Sources/Images/free-delivery-1-1.png";
+import img6 from "../../Sources/Images/grocery.png";
+import testimonial from "../../Sources/Images/buy-fresh-fruits-online.jpg";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="fancy-title">
+        Fresh Fruits Online & Fresh Vegetables Online
+      </h1>
+      <div className="home-container">
+        <div className="fancy">
+          <p className="fancy-para">
+            We’re not your average fruit importers. We have expertise in
+            sourcing the choicest of fresh fruits from around the world and
+            distributing it across the length and breadth of India. Our
+            top-notch distribution network strengthened through almost a century
+            of existence in the fruit business, works efficiently to ensure the
+            fruits are delivered far and wide with an intriguing depth of
+            freshness. We provide fresh fruits online and fresh vegetables
+            online.
+          </p>
+          <p className="clearboth" style={{ fontSize: "20px" }}>
+            {" "}
+            We have expertise in providing fresh fruits online and fresh
+            vegetables online and offline and sourcing the choicest of fruits
+            and vegetables from around the world
+          </p>
+        </div>
+        {/* Product section */}
+        <div className="home-veg-prod">
+          <p><img src={img1} alt="images"></img><br></br>
+          <b>Vegetables</b><br></br>
+          <button className="btn btn-success">Click Here</button></p>
+          <p><img src={img2} alt="images"></img><br></br>
+          <b>Fruits</b><br></br>
+          <button className="btn btn-success">Click Here</button></p>
+          <p><img src={img3} alt="images"></img><br></br>
+          <b>Seeds</b><br></br>
+          <button className="btn btn-success">Click Here</button></p>
+          <p><img className="down-fruits" src={img4} alt="images"></img><br></br>
+          <b>Beverage</b></p>
+          <p><img className="down-fruits" src={img5} alt="images"></img><br></br>
+          <b>Shipping</b></p>
+          <p><img className="down-fruits" src={img6} alt="images"></img><br></br>
+          <b>Supply</b></p>
+        </div>
+      </div>
+      <div className="bar"></div>
+      <div className="testimonials">
+        <img src={testimonial} alt="images"></img>
+      </div>
+      <div className="info">
+        <div className="left">
+          <p>Final Sales</p>
+
+          <h1>Get More with Les</h1>
+          <button className="btn btn-info">All Products</button>
+        </div>
+        <div className="right">
+          <p>Hurry up Before the Stock Ends</p>
+
+          <h1>Online Order</h1>
+          <button className="btn btn-info">Get it now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,65 @@
+import React from "react";
+import './Header.css'
+import { Link } from "react-router-dom";
+// Carousel
+import Carousel from "react-bootstrap/Carousel";
+import slide1 from "../../../Sources/Images/fresh-fruit-delivery-in-gurgaon.jpg";
+import slide2 from "../../../Sources/Images/online-vegetables-in-gurgaon.jpg";
+import slide3 from "../../../Sources/Images/online-vegetables-in-gurgaon.jpg";
+// navbar
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from '../../../Sources/Images/logo-est.png'
+
+
+const Header = () => {
+  return (
+    <div>
+    {/* NavBAr START*/}
+    <>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand as={Link} to="/"><img src={logo} alt='images'></img></Navbar.Brand>
+          <Nav>
+            <Nav.Link as={Link} to="/home">Home</Nav.Link>
+            <Nav.Link as={Link} to="/About">About</Nav.Link>
+            <Nav.Link as={Link} to="/registration">Registration</Nav.Link>
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+    {/* NavBAr END */}
+      <Carousel>
+        <Carousel.Item>
+          <img className="d-block w-100" src={slide1} alt="First slide" />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={slide2} alt="Second slide" />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={slide1} alt="Third slide" />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Header;
